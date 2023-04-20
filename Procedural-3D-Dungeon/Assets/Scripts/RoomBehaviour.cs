f7f80c8afe89ed76e7dd5d74d64367923544c33e
FFerorelli <fabrizio.ferorelli@gmail.com>
2023-04-20 17:42:41 +0200

    Maze generation

--- Procedural-3D-Dungeon/Assets/Scripts/RoomBehaviour.cs
+++ Procedural-3D-Dungeon/Assets/Scripts/RoomBehaviour.cs
@@ -1,13 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomBehaviour : MonoBehaviour
 {
+    // 0 - up
+    // 1 - Down
+    // 0 - Right
+    // 0 - Left
+    public GameObject[] walls;
+    public GameObject[] doors;
+   // public bool[] testStatus;
+
     // Start is called before the first frame update
-    void Start()
+    //void Start()
+    //{
+    //   UpdateRoom(testStatus);
+    //}
+
+   public void UpdateRoom(bool[] status)
     {
-        
+        for (int i = 0; i < status.Length; i++)
+        {
+            doors[i].SetActive(status[i]);
+            walls[i].SetActive(!status[i]);
+        }
     }
 
     // Update is called once per frame
